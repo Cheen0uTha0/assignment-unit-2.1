e1cb9f8b18613ca2ab9e56ace6313f9c1b606f7e
Cheenou Thao <cheenou_thao10@hotmail.com>
2023-11-05 20:15:48 -0600

    Changes to introduction

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -15,13 +15,17 @@ console.log( fullName );
 
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
-
+let luckyNumber = 14
+console.log(luckyNumber);
 
 // 5 - Create a variable `introduction` and using the variables from above,
 //     give it the value of:
 //     'My name is (full name), and I think (lucky number) is a winner!'.
 //     Refer back to the videos if you need help with this one.
 //     Console log the value of `introduction`
+let introduction
+ console.log('My name is', fullName, 'and I think', luckyNumber, 'is a winner!');
+
 
 // 6 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 //     Console log the value of `adventurous`
